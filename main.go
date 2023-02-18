c9957291d5bcfd7fb4b30d5920591de123a278b7
Sathish Kumar Renganathan <sk015x@gmail.com>
2023-02-18 11:59:38 -0600

    added datamode

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"igredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
 
 func IndexHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
